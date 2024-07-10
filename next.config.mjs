aaeed6494bbaf938bd0373205227266c71474a8d
Andrii Panasenko <130774640+Andrij-Panasenko@users.noreply.github.com>
2024-07-10 14:02:51 +0300

    upd next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    SECRET: process.env.SECRET,
+    NEXT_URL: process.env.NEXT_URL,
+  },
+};
 
 export default nextConfig;
