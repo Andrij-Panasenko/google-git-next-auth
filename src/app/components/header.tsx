6fe3d71aeb54c20dede70ccede87f31dde505ffd
Andrii Panasenko <130774640+Andrij-Panasenko@users.noreply.github.com>
2024-07-11 11:18:04 +0300

    create header component

---
+++ src/app/components/header.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import React from 'react';
+
+export interface HeaderProps {
+  children: React.ReactNode;
+}
+
+export default function Header({ children }: HeaderProps) {
+  return (
+    <>
+      <header className=" container m-auto flex items-center gap-5 py-6	px-10 border-b border-l border-r border-gray-300">
+        <h1 className="flex-1 text-3xl font-semibold text-gray-900">
+          {children}
+        </h1>
+        <div className="w-px self-stretch bg-gray-300" />
+
+        <div className="flex gap-3 items-center">
+          <Image
+            width={44}
+            height={44}
+            src="/images/avatar.png"
+            alt="User avatar"
+          />
+          <p className="text-base	font-semibold text-gray-900">John Smith</p>
+        </div>
+      </header>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AuthForm from './components/auth-form';
+import Header from './components/header';
 
 export default function Home() {
   return (
     <main>
-      <AuthForm />
+      {/* <AuthForm /> */}
+      <Header>My Tipaw</Header>
     </main>
   );
 }
