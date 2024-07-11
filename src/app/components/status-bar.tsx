b8f1a7216b0a6169b77663653d6577e03d47ddfb
Andrii Panasenko <130774640+Andrij-Panasenko@users.noreply.github.com>
2024-07-11 12:37:22 +0300

    styling for full status-bar

--- src/app/components/status-bar.tsx
+++ src/app/components/status-bar.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 export interface StatusBarProps {
@@ -7,20 +8,66 @@ export interface StatusBarProps {
 export default function StatusBar({ children }: StatusBarProps) {
   return (
     <>
-      <div className="container m-auto mt-10 py-6 px-10 bg-white flex justify-between">
-        <div className="flex flex-col gap-3">{children}</div>
-        <div className="flex flex-col justify-between">
-          <div className="flex justify-between">
-            <p className="font-bold">Profile compleated</p>
-            <p className="text-emerald-500 font-bold">100%</p>
+      <div className="container m-auto mt-10 py-6 px-10">
+        <div className="flex justify-between bg-white border-b border-gray-300 py-6 px-10">
+          <div className="flex flex-col gap-3">{children}</div>
+          <div className="flex flex-col justify-between">
+            <div className="flex justify-between">
+              <p className="font-bold">Profile compleated</p>
+              <p className="text-emerald-500 font-bold">100%</p>
+            </div>
+            <div className="flex gap-1">
+              <span className="block h-2 w-20 bg-green-300"></span>
+              <span className="block h-2 w-20 bg-green-300"></span>
+              <span className="block h-2 w-20 bg-green-300"></span>
+              <span className="block h-2 w-20 bg-green-300"></span>
+              <span className="block h-2 w-20 bg-green-300"></span>
+            </div>
           </div>
-          <div className="flex gap-1">
-            <span className="block h-2 w-20 bg-green-300"></span>
-            <span className="block h-2 w-20 bg-green-300"></span>
-            <span className="block h-2 w-20 bg-green-300"></span>
-            <span className="block h-2 w-20 bg-green-300"></span>
-            <span className="block h-2 w-20 bg-green-300"></span>
-          </div>
+        </div>
+        <div className="bg-gray-100 py-12 px-8">
+          <ul className="flex gap-10">
+            <li className=" flex-grow bg-white space-y-5 p-6 shadow-lg shadow-gray-300/50">
+              <Image
+                width={16}
+                height={16}
+                src="/icons/check.svg"
+                alt="check icon"
+              />
+              <h3 className="font-bold">Verification</h3>
+              <p className="text-gray-600">You verifyed your account</p>
+            </li>
+            <li className=" flex-grow bg-white space-y-5 p-6 shadow-lg shadow-gray-300/50">
+              <Image
+                width={16}
+                height={16}
+                src="/icons/check.svg"
+                alt="check icon"
+              />
+              <h3 className="font-bold">Profile picture</h3>
+              <p className="text-gray-600">You added profile picture</p>
+            </li>
+            <li className=" flex-grow bg-white space-y-5 p-6 shadow-lg shadow-gray-300/50">
+              <Image
+                width={16}
+                height={16}
+                src="/icons/check.svg"
+                alt="check icon"
+              />
+              <h3 className="font-bold">Parents</h3>
+              <p className="text-gray-600">Ypu added the parents</p>
+            </li>
+            <li className=" flex-grow bg-white space-y-5 p-6 shadow-lg shadow-gray-300/50">
+              <Image
+                width={16}
+                height={16}
+                src="/icons/check.svg"
+                alt="check icon"
+              />
+              <h3 className="font-bold">Litter</h3>
+              <p className="text-gray-600">Ypu added the litter</p>
+            </li>
+          </ul>
         </div>
       </div>
     </>
